47f55b90b2dd9579cb5a517750f1d8960403dbb6
jakubfraszczak1-max <jakub.fraszczak1@gmail.com>
2026-07-12 12:06:17 +0200

    Delete pizzeria-planeta-main directory

--- pizzeria-planeta-main/README.md
+++
@@ -1,2 +0,0 @@
-# pizzeria-planeta
-pizzeria-planeta
--- pizzeria-planeta-main/docs/start-server.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Uruchamianie serwera Pizzeria Planeta...
-echo Otworz: http://localhost:8080
-echo.
-python -m http.server 8080
-pause
